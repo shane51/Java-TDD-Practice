5a5b4353fabea819ee92d58fc885247d3dc214f1
shane51 <suse51@163.com>
2016-03-10 10:33:34 +0800

    add get All students tests

--- Agile_java/src/CourseSessionTest.java
+++ Agile_java/src/CourseSessionTest.java
@@ -14,9 +14,16 @@ public class CourseSessionTest extends junit.framework.TestCase {
         Student student1 = new Student("Cain DiVoe");
         session.enroll(student1);
         assertEquals(1, session.getNumberOfStudents());
+        java.util.ArrayList<Student> allStudents = session.getAllStudents();
+        assertEquals(1, allStudents.size());
+        assertEquals(student1,allStudents.get(0));
 
         Student student2 = new Student("Coralee DeVaughn");
         session.enroll(student2);
         assertEquals(2, session.getNumberOfStudents());
+        assertEquals(2, allStudents.size());
+        assertEuqals(student1, allStudents.get(0));
+        assertEuqals(student2, allStudents.get(1));
+
     }
 }
